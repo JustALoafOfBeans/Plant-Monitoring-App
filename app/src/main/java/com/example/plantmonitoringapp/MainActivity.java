044603a4a6de94d5063950a779579b426a6ffa07
JustALoafOfBeans <victoriachen4520@gmail.com>
2024-03-21 23:41:19 -0400

    resolve image names, add connection to home page

--- app/src/main/java/com/example/plantmonitoringapp/MainActivity.java
+++ app/src/main/java/com/example/plantmonitoringapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.plantmonitoringapp;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Instantiate button
+    private AppCompatButton start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (AppCompatButton) findViewById(R.id.startButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, homeActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
