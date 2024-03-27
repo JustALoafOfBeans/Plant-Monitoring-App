5d88250eb3e5138cc2502e27810bbbcfed73d370
JustALoafOfBeans <victoriachen4520@gmail.com>
2024-03-26 20:01:18 -0400

    Able to anonymously connect to database on launch

--- app/src/main/java/com/example/plantmonitoringapp/MainActivity.java
+++ app/src/main/java/com/example/plantmonitoringapp/MainActivity.java
@@ -5,20 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.net.ipsec.ike.TunnelModeChildSessionParams;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
+
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
 
 public class MainActivity extends AppCompatActivity {
 
     // Instantiate button
     private AppCompatButton start;
+    String AppID = "application-0-giedz";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize and connect to MongoDB realm database
+        Realm.init(this);
+        App app = new App(new AppConfiguration.Builder(AppID).build());
+
+        Credentials credentials = Credentials.anonymous();
+        app.loginAsync(Credentials.anonymous(), new App.Callback<User>() {
+            @Override
+            public void onResult(App.Result<User> result) {
+                if(result.isSuccess()) {
+                    Log.v("User","User logged in anonymously");
+                } else {
+                    Log.v("User", "Failed to log in");
+                }
+            }
+        });
+
+        // Initialize button to home screen
         start = (AppCompatButton) findViewById(R.id.startButton);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/plantmonitoringapp/homeActivity.java
+++ app/src/main/java/com/example/plantmonitoringapp/homeActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoDatabase;
+
 public class homeActivity extends AppCompatActivity {
+
+    MongoDatabase database;
+    MongoClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
