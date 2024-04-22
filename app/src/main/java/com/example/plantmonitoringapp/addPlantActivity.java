3855d37e4614d910c8c02955dd61c8f10e16329f
JustALoafOfBeans <victoriachen4520@gmail.com>
2024-04-22 17:51:16 -0400

    dropdown able to populat fields accordingly

--- app/src/main/java/com/example/plantmonitoringapp/addPlantActivity.java
+++ app/src/main/java/com/example/plantmonitoringapp/addPlantActivity.java
@@ -3,14 +3,20 @@ package com.example.plantmonitoringapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.google.android.material.shape.InterpolateOnScrollPositionChangeHelper;
 
 import org.bson.Document;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -18,25 +24,85 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import io.realm.mongodb.App;
 import io.realm.mongodb.RealmResultTask;
 import io.realm.mongodb.mongo.MongoCollection;
 import io.realm.mongodb.mongo.iterable.MongoCursor;
 
 public class addPlantActivity extends AppCompatActivity {
     private Spinner dropdown;
+    private AppCompatButton add_btn;
+    private EditText target_moist,min_moist,freq,light;
     Map<String,String> viable_plants = new HashMap<String,String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
+
+        dropdown = (Spinner) findViewById(R.id.type_dropdown);
+        add_btn = (AppCompatButton) findViewById(R.id.add_btn);
+        target_moist = (EditText) findViewById(R.id.ui_target_moist);
+        min_moist = (EditText) findViewById(R.id.ui_min_moist);
+        freq = (EditText) findViewById(R.id.ui_water_interval);
+        light = (EditText) findViewById(R.id.ui_light);
+
+        /* Trigger when new item selected in drop down
+         */
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object raw = parent.getItemAtPosition(position);
+                String selected = raw.toString();
+                populateOptions(selected);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // do nothing
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        // Reset
+        viable_plants.clear();
+        viable_plants.put("custom","");
         getData();
+    }
+
+    /* When option in spinner is selected, populate fields accordingly
+     * Input: String of item selected
+     * Return: void
+     */
+    private void populateOptions(String selected) {
+        if (selected.equals("custom")) {
+            // Clear options
+            light.getText().clear();
+            min_moist.getText().clear();
+            target_moist.getText().clear();
+            freq.getText().clear();
+        } else {
+            String vals = viable_plants.get(selected);
+            try {
+                JSONObject toJSON = new JSONObject(vals);
+                int lightJSON = toJSON.getInt("light_schedule");
+                int min_moistJSON = toJSON.getInt("moisture_lowerbound");
+                int max_moistJSON = toJSON.getInt("moisture_upperbound");
+                int intervalJSON = toJSON.getInt("water_interval");
+
+                //Populate fields
+                light.setText(String.valueOf(lightJSON));
+                min_moist.setText(String.valueOf(min_moistJSON));
+                target_moist.setText(String.valueOf(max_moistJSON));
+                freq.setText(String.valueOf(intervalJSON));
+
+            } catch (JSONException e) {
+                Log.v("Error", e.toString());
+            }
+        }
     }
 
     /* Pull all plant types from plant database and append to array list
@@ -63,16 +129,18 @@ public class addPlantActivity extends AppCompatActivity {
         });
     }
 
-    /* Populate drop down/spinner with items
+    /* Populate drop down/spinner with items and set initial value
      */
     private void populateSpinner() {
-        dropdown = (Spinner) findViewById(R.id.type_dropdown);
-
-        viable_plants.put("custom",""); // add custom option
         Set<String> getKeys = viable_plants.keySet();
         String[] keys = getKeys.toArray(new String[getKeys.size()]);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,keys);
         dropdown.setAdapter(adapter);
+
+        // Set initial value to "custom"
+        ArrayAdapter x = (ArrayAdapter) dropdown.getAdapter();
+        int custom_pos = x.getPosition("custom");
+        dropdown.setSelection(custom_pos);
     }
 }
