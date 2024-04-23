363d8bba09a0d66bd54a93c604296d5e470a255e
JustALoafOfBeans <victoriachen4520@gmail.com>
2024-04-23 02:54:32 -0400

    able to add new plant if using preset option

--- app/src/main/java/com/example/plantmonitoringapp/addPlantActivity.java
+++ app/src/main/java/com/example/plantmonitoringapp/addPlantActivity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -32,7 +33,8 @@ import io.realm.mongodb.mongo.iterable.MongoCursor;
 public class addPlantActivity extends AppCompatActivity {
     private Spinner dropdown;
     private AppCompatButton add_btn;
-    private EditText target_moist,min_moist,freq,light;
+    private EditText name,target_moist,min_moist,freq,light;
+    private String selected;
     Map<String,String> viable_plants = new HashMap<String,String>();
 
     @Override
@@ -42,10 +44,22 @@ public class addPlantActivity extends AppCompatActivity {
 
         dropdown = (Spinner) findViewById(R.id.type_dropdown);
         add_btn = (AppCompatButton) findViewById(R.id.add_btn);
+        name = (EditText) findViewById(R.id.ui_plant_name);
         target_moist = (EditText) findViewById(R.id.ui_target_moist);
         min_moist = (EditText) findViewById(R.id.ui_min_moist);
         freq = (EditText) findViewById(R.id.ui_water_interval);
         light = (EditText) findViewById(R.id.ui_light);
+
+        /* Enable add button
+         */
+        add_btn = (AppCompatButton) findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check unique plant name then add it if unique
+                checkViableName();
+            }
+        });
 
         /* Trigger when new item selected in drop down
          */
@@ -53,7 +67,7 @@ public class addPlantActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Object raw = parent.getItemAtPosition(position);
-                String selected = raw.toString();
+                selected  = raw.toString();
                 populateOptions(selected);
             }
             @Override
@@ -71,6 +85,67 @@ public class addPlantActivity extends AppCompatActivity {
         viable_plants.clear();
         viable_plants.put("custom","");
         getData();
+    }
+
+    private void addCustomPlant() {
+        Log.v("Data","Add custom plant");
+        //todo handle empty edittext
+        int new_target_moist = Integer.parseInt(target_moist.getText().toString());
+        int new_min_moist = Integer.parseInt(min_moist.getText().toString());
+        int new_freq = Integer.parseInt(freq.getText().toString());
+        int new_light = Integer.parseInt(light.getText().toString());
+    }
+
+    private void checkViableName() {
+        MongoCollection<Document> collection = Connection.getUserCollection();
+        ArrayList<String> names = new ArrayList<String>(1);
+        String new_name = name.getText().toString();
+
+        //Place all plant names in array
+        RealmResultTask<MongoCursor<Document>> findTask = collection.find().iterator();
+        findTask.getAsync(task -> {
+            if(task.isSuccess()) {
+                MongoCursor<Document> results = task.get(); //cursor to iterate through documents
+                while(results.hasNext()) {
+                    Document doc = results.next();
+                    names.add(doc.getString("name"));
+                    // Log.v("Data",doc.toJson()); // for debugging
+                }
+                // Check if name unique
+                if (names.contains(new_name)) {
+                    Toast.makeText(addPlantActivity.this,"Plant names must be unique.",Toast.LENGTH_LONG).show();
+                } else if (new_name.equals(selected)){
+                    // if name is unique and custom selected
+                    addCustomPlant();
+                } else {
+                    addplant();
+                }
+            } else {
+                Log.v("Data","Failed to return documents.");
+            }
+        });
+    }
+
+    /* Insert plant to database
+     */
+    private void addplant() {
+        Log.v("Data","Adding new "+selected);
+        String new_name = name.getText().toString();
+
+        //Insert a document
+        //.append format ("field name","data in field")
+        MongoCollection<Document> collection = Connection.getUserCollection();
+        Document doc = new Document().append("name",new_name).append("plant_type",selected.replace(" ","_"));
+        collection.insertOne(doc).getAsync(result -> {
+            if(result.isSuccess()) {
+                Log.v("Data","Inserted");
+                // Return to home activity
+                Intent intent = new Intent(addPlantActivity.this, homeActivity.class);
+                startActivity(intent);
+            } else {
+                Log.v("Data","Failed to insert");
+            }
+        });
     }
 
     /* When option in spinner is selected, populate fields accordingly
@@ -98,6 +173,8 @@ public class addPlantActivity extends AppCompatActivity {
                 min_moist.setText(String.valueOf(min_moistJSON));
                 target_moist.setText(String.valueOf(max_moistJSON));
                 freq.setText(String.valueOf(intervalJSON));
+
+                //todo lock options if using defaults
 
             } catch (JSONException e) {
                 Log.v("Error", e.toString());
